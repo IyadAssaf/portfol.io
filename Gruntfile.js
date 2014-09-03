0960a260a9e9ccd0af471a2d0f83aa09c11889d2
iyadassaf <iyadassaflondon@gmail.com>
2014-09-04 00:53:57 +0100

    Added to heroku-sass support with reference to this: https://github.com/glebm/heroku-buildpack-nodejs-grunt-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -160,6 +160,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-mkdir');
+	grunt.registerTask('heroku:production', 'clean less mincss uglify');
+	grunt.registerTask('heroku:development', 'clean less mincss');
 
     // Tasks
 	grunt.registerTask('sassMin', ['sass', 'cssmin']);
